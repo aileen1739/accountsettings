4cfb5e89fa2b3ffede314ee353b77a196391cc3d
Aileen <acarrillo78@toromail.csudh.edu>
2024-04-16 22:41:43 -0700

    Account Settings Navigation bar and Css styles added

---
+++ src/AccountSettings.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const AccountSettings = () => {
+    return(
+        <main>
+        <div class="container">
+            <div class="box">
+                <h1>Account Settings</h1>
+                <div class="profile">
+                    <span class="material-symbols-outlined">
+                        account_circle
+                    </span>
+                </div>
+                <ul class="menu">
+                    <li><a href="#">Account Info</a></li>
+                    <li><a href="#">Change Password</a></li>
+                    <li><a href="#">Orders</a></li>
+                    <li><a href="#">Sales</a></li>
+                    <li><a href="#">Go Back</a></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+    );
+}
+
+export default AccountSettings;
